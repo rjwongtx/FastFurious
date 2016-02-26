9c3886a081827b07713ba646f7256d70bf5ce5db
Michael <michaelyoung8000@gmail.com>
2016-02-26 17:44:39 -0600

    Update Movement.java

--- Movement.java
+++ Movement.java
@@ -1,1 +1,19 @@
-//hi micheal 
+import rxtxrobot.*;
+
+public class movement
+{
+  public static void main(String args[])
+  {
+    robot.setPort("COM3");
+    robot.connet();
+    
+    robot.attachMotor(RXTXRobot.MOTOR1, x); //x is a standin and will be replaced with the actual port number
+    robot.attachMotor(RXTXRobot.MOTOR2, y); //y is a standin and will be replaced with the actual port number 
+    
+    robot.runMotor(RXTXRobot.MOTOR1, 250, 3000); //half power for 3 seconds to get an estimate on speeed
+    robot.runMotor(RXTXRobot.MOTOR2, 250, 3000); 
+    
+    robot.close();
+  }
+  
+}
