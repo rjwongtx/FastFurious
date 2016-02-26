8335f905ed00d3d738c1eb0026061f263df0f201
rjwongtx <rjwong@smu.edu>
2016-02-26 16:36:54 -0600

    Added two more classes, eidited third

---
+++ PingSensor.java
@@ -0,0 +1,20 @@
+import rxtxrobot.*;
+
+public class PingSensor
+{
+	static RXTXRobot robot = new ArduinoUno();
+	static int ping = 12;
+
+	public static void main(String[] args)
+	{
+		robot.setPort("COM3"); // Set the port to COM3
+		robot.connect();
+		for (int x=0; x < 100; ++x)
+		{
+			//Read the ping sensor value, which is connected to pin 12
+			System.out.println("Response: " + robot.getPing(ping) + " cm");
+			robot.sleep(300);
+		}
+		robot.close();
+	}
+} 
