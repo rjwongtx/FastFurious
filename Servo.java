8b671e82c15deac214f91fb1102fea7053d83036
Michael <michaelyoung8000@gmail.com>
2016-02-27 14:20:48 -0600

    Update Servo.java

--- Servo.java
+++ Servo.java
@@ -1,1 +1,20 @@
-//micheal 
+import rxtxrobot.*;
+
+public class movement
+{
+  public static void main(String args[])
+  {
+    robot.setPort("COM3");
+    robot.connet();
+    
+    robot.attachServo(RXTXRobot.SERVO1, x); //x is a standin and will be replaced with the actual port number
+    robot.attachServo(RXTXRobot.SERVO2, y); //y is a standin and will be replaced with the actual port number 
+    
+    
+    robot.moveServo(RXTXRobot.SERVO1, 180); //half power for 3 seconds to get an estimate on speeed
+    robot.moveServo(RXTXRobot.SERVO2, 180); 
+    
+    robot.close();
+  }
+  
+}
